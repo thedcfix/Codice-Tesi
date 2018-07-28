7f74a12f4ea0cd3d4c8da82d9ce159bcd5d6809b
Davide <davide.cattaneo.it@gmail.com>
2018-07-28 23:36:34 +0200

    plot

---
+++ plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import shapefile as shp
+
+df = pd.read_csv('result.csv',sep=';', decimal='.')
+sf = shp.Reader("shapefile/Regione_polygon.shp")
+
+X = df["UTM_Est"]
+Y = df["UTM_Nord"]
+Z = df["Valore"]
+
+plt.figure()
+
+for shape in sf.shapeRecords():
+    x = [i[0] for i in shape.shape.points[:]]
+    y = [i[1] for i in shape.shape.points[:]]
+    plt.plot(x,y)
+
+
+plt.scatter(X,Y,edgecolors='none',c=Z)
+plt.colorbar()
+plt.show()
