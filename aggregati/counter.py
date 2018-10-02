07703e534d4697410510bdb42dc12cafd1b39dab
Davide <davide.cattaneo.it@gmail.com>
2018-10-02 21:31:24 +0200

    incidenza excel

--- aggregati/counter.py
+++ aggregati/counter.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
+year = 2017
+
+print("Data for year", year)
+
 data = pd.read_csv('tabella.csv',sep=',', decimal='.')
 data = data.loc[data.CD_PROVINCE.isin(['BG', 'BS', 'CO', 'CR', 'LC', 'LO', 'MB', 'MI', 'MN', 'PV', 'SO', 'VA'])]
 
-data = data.loc[data.ANNO == 2017]
+data = data.loc[data.ANNO == year]
 
-data = data.groupby(['CD_PROVINCE']).count()
+data = data.groupby(['CD_PROVINCE', 'AC_SESSO']).count()
 
 to_keep = ['AAT']
 data = data[to_keep]
