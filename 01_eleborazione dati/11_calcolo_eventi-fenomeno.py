108b287df1019d73e08779a42a465e78ede9031b
Davide <davide.cattaneo.it@gmail.com>
2018-11-11 22:43:19 +0100

    update

--- 01_eleborazione dati/11_calcolo_eventi-fenomeno.py
+++ 01_eleborazione dati/11_calcolo_eventi-fenomeno.py
@@ -2,26 +2,28 @@ import pandas as pd
 import time
 import sys
 
-data = pd.read_csv('CONCLUSIONE_2015.csv',sep=';', decimal=',')
+data1 = pd.read_csv('CONCLUSIONE_2015.csv',sep=';', decimal=',')
+data2 = pd.read_csv('CONCLUSIONE_2016.csv',sep=';', decimal=',')
+data3 = pd.read_csv('CONCLUSIONE_2017.csv',sep=';', decimal=',')
+
+year = 2015
+
+frames = [data1, data2, data3]
+data = pd.concat(frames)
 data = data.loc[data.DS_TOWN == "MILANO"]
-data.to_csv("CONCLUSIONE_2015.csv", sep=';', decimal=',', header=True, index=False)
+data = data.loc[data.ANNO == year]
+
+data.to_csv("CONCLUSIONE.csv", sep=';', decimal=',', header=True, index=False)
 
 # calcolo ictus e media per ogni giorno
-data["DATE_TIME_x"] = pd.to_datetime(data["DATE_TIME_x"])
-data["DAY"] = data.DATE_TIME_x.dt.day
+data["DATE_TIME"] = pd.to_datetime(data["DATE_TIME"])
+data["DAY"] = data.DATE_TIME.dt.day
 
-src = data
-
-numero = data.groupby(['ANNO', 'MESE', 'DAY'])["DATE_TIME_x"].size()
+numero = data.groupby(['ANNO', 'MESE', 'DAY'])["DATE_TIME"].size()
 media = data.groupby(['ANNO', 'MESE', 'DAY'])[str(sys.argv[1])].mean()
 
 data = pd.DataFrame({'EVENTI': numero, 'MEDIA '+str(sys.argv[1]): media})
 data.to_csv("OUT_GIORNO.csv", sep=';', decimal=',', header=True)
 
-# calcolo ictus e media per specifico datetime
-# data = src
-# numero = data.groupby(["DATE_TIME_x"]).size()
-# media = data.groupby(["DATE_TIME_x"])[str(sys.argv[1])].mean()
-
-# data = pd.DataFrame({'EVENTI': numero, 'MEDIA '+str(sys.argv[1]): media})
-# data.to_csv("OUT_TIMESTAMP.csv", sep=';', decimal=',', header=True)
+#print("Correlazione con", str(sys.argv[1]), "anno", year, ":", data["EVENTI"].corr(data['MEDIA '+str(sys.argv[1])]))
+print(round(data["EVENTI"].corr(data['MEDIA '+str(sys.argv[1])]), 3))
