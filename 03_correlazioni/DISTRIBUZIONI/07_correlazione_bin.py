cdc8e44ee13c3be672c8436b91ce2814c930d45b
Davide <davide.cattaneo.it@gmail.com>
2018-11-27 22:02:19 +0100

    update

---
+++ 03_correlazioni/DISTRIBUZIONI/07_correlazione_bin.py
@@ -0,0 +1,31 @@
+import pandas as pd
+data = pd.read_csv('CORR_PM25.csv',sep=';', decimal=',')
+import time
+
+step = 5
+base = 0
+top = 150
+current = base
+
+dict = {}
+count = {}
+current = base
+
+while True:
+	if current >= top:
+		break
+
+	out = data.query(str(current) + ' < V0 < ' + str(current + step))
+	tot = out.COUNT.sum()
+	cnt = out.V0.count()
+	current += step
+	dict[current + step/2] = tot
+	count[current + step/2] = cnt
+	#print("Range:", current-1, current, tot, out)
+	
+print(dict)
+out = pd.DataFrame(list(dict.items()), columns=['BIN', 'COUNT'])
+out.to_csv("OUT__.csv", sep=';', decimal=',', header=True, index=False)
+
+cnt = pd.DataFrame(list(count.items()), columns=['BIN', 'AVG'])
+cnt.to_csv("OUT__CNT.csv", sep=';', decimal=',', header=True, index=False)
